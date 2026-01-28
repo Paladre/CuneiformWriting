6655da5c3fe57818f9ff343e5c966702eaa9be0f
Paladre <adrien.clair17@yahoo.com>
2026-01-28 23:23:50 +0100

    moved loadstrokes there to clean up

---
+++ CuneiformWriting/Utils/StrokesUtils.cs
@@ -0,0 +1,56 @@
+﻿using CuneiformWriting.Gui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+
+namespace CuneiformWriting.Utils
+{
+    public static class StrokesUtils
+    {
+        public static List<CuneiformStroke> LoadStrokes(ItemStack stack)
+        {
+            List<CuneiformStroke> result = new List<CuneiformStroke>();
+
+            byte[] bytes = stack.Attributes.GetBytes("cuneiform");
+
+            if (bytes == null || bytes.Length == 0)
+            {
+                return result;
+            }
+
+            TreeAttribute tree;
+
+            try
+            {
+                tree = TreeAttribute.CreateFromBytes(bytes);
+            }
+            catch
+            {
+                return result;
+            }
+
+            int count = tree.GetInt("count");
+
+            for (int i = 0; i < count; i++)
+            {
+                CuneiformStroke s = new CuneiformStroke();
+
+                s.x1 = tree.GetFloat("x1" + i);
+                s.y1 = tree.GetFloat("y1" + i);
+                s.x2 = tree.GetFloat("x2" + i);
+                s.y2 = tree.GetFloat("y2" + i);
+                s.thicknessDelta = tree.GetFloat("thicknessDelta" + i);
+                s.typeofstroke = (StrokeType)tree.GetInt("t" + i, (int)StrokeType.stroke);
+
+                result.Add(s);
+            }
+
+            return result;
+        }
+    }
+    
+}
